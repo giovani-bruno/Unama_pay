d8e334b5f8efd8ab3f31bb07fe861e8c6f12a772
Numl8ck <giovainic@gmail.com>
2024-05-31 12:55:17 -0300

    removido ícone de debug da appbar

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Unama Pay',
       theme: ThemeData(
         hintColor: const Color.fromARGB(255, 0, 0, 0),
